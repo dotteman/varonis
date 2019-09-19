38f54dc9bfeaae0faef23af97bef488da364b6c7
Dave Otteman <dave.otteman@gmail.com>
2019-09-19 15:25:45 -0700

    adding scrript used to create the storage account, resource group, and container

---
+++ createVaronisStorage.ps1
@@ -0,0 +1,24 @@
+﻿
+# sign-in to Azure
+Connect-AzAccount
+
+#set location variable
+$location = "westus"
+
+#create resource group
+$resourcegroup = "VaronisCodeTest"
+New-AzResourceGroup -Name $resourcegroup -Location $location
+
+#create storage account
+$storageaccount = New-AzStorageAccount -ResourceGroupName $resourcegroup -Name "varonisstorageaccount" -SkuName Standard_LRS -Location $location 
+
+$storageaccountcontext = $storageaccount.Context
+
+#create container
+$containername = "logfileblobs"
+New-AzStorageContainer -Name $containername -Context $storageaccountcontext -Permission Blob
+
+
+
+
+
